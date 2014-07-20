a33029a04212c3e0b535076c6fd6b111752005cc
cpancake <anrogers529@gmail.com>
2014-07-20 18:38:06 -0400

    Version bump

--- saefacepunch.user.js
+++ saefacepunch.user.js
@@ -3,9 +3,10 @@
 // @description Something Awful emotes on Facepunch.
 // @author cpancake
 // @downloadURL http://github.com/cpancake/SAEFacepunch
+// @updateURL https://github.com/cpancake/SAEFacepunch/raw/master/saefacepunch.user.js
 // @include http://facepunch.com/*
 // @include http://www.facepunch.com/*
-// @version 0.1
+// @version 0.2
 // @copyright 2014 cpancake
 // @namespace http://github.com/cpancake/SAEFacepunch
 // ==/UserScript==
